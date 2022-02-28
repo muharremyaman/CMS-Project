34a590033115c4049b959714f6b458ae35ade4ba
Muharrem Yaman <muharrem2022@gmail.com>
2022-02-28 21:47:42 +0300

    folder for dashboard_v created. Empty content page created

---
+++ panel/application/views/includes/footer.php
@@ -0,0 +1,12 @@
+
+
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+      <div class="clearfix">
+        <ul class="footer-menu pull-right">
+        
+        </ul>
+        <div class="copyright pull-left">Tüm Hakları Saklıdır |  Muharrem Yaman <?php echo date("Y"); ?> &copy;</div>
+      </div>
+    </footer>
+  </div>
