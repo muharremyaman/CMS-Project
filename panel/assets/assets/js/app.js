f64863f3c34377f55f9fa9a3ced3305ec55b7498
Muharrem Yaman <muharrem2022@gmail.com>
2022-03-07 19:10:11 +0300

    Colors of theme have changed

--- panel/assets/assets/js/app.js
+++ panel/assets/assets/js/app.js
@@ -554,6 +554,12 @@
 // initialize app
 +function($, window) { 'use strict';
 	window.app.init();
+	window.app.menubar.setTheme('dark');
+	window.app.menubar.applyTheme();
+	
+	window.app.navbar.setTheme('warning');
+	window.app.navbar.applyTheme();
+
 	window.app.menubar.init();
 	window.app.navbar.init();
 	window.app.customizer.init();
