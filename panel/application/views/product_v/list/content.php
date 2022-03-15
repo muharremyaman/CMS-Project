22a51c3031ca5b5031130fa02668189945aca0e3
Muharrem Yaman <muharrem2022@gmail.com>
2022-03-15 19:42:13 +0300

    Html template created for products module's product list

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,58 @@
-Ürün Listesi Content
+a<div class="row"> 
+    	<div class="col-md-12">
+				<h4 class="m-b-lg">Ürün Listesi</h4>
+			</div><!-- END column -->
+        <div class="col-md-12">
+				<div class="widget p-lg">
+				
+					<table class="table table-hover table-striped">
+					
+                    <thead>
+                        <th>#id</th>
+                        <th>URL</th>
+                        <th>Başlık</th>
+                        <th>Açıklama</th>
+                        <th>Durumu</th>
+                        <th>İşlem</th>
+                        
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>#1</td>
+                            <td>monitor-askisi</td>
+                            <td>Monitör Askısı</td>
+                            <td>360 derece kullanabilen monitör askısıdır</td>
+                            <td>1/0</td>
+                            <td>
+                                <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
+                                <a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
+                            </td>
+                                                </tr>
+                        <tr>
+                            <td>#1</td>
+                            <td>monitor-askisi</td>
+                            <td>Monitör Askısı</td>
+                            <td>360 derece kullanabilen monitör askısıdır</td>
+                            <td>1/0</td>
+                            <td>
+                                <a href="#">Sil /</a>
+                                <a href="#">Düzenle</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>#1</td>
+                            <td>monitor-askisi</td>
+                            <td>Monitör Askısı</td>
+                            <td>360 derece kullanabilen monitör askısıdır</td>
+                            <td>1/0</td>
+                            <td>
+                                <a href="#">Sil /</a>
+                                <a href="#">Düzenle</a>
+                            </td>
+                        </tr>
+                    </tbody>
+
+					</table>
+				</div><!-- .widget -->
+			</div><!-- END column -->
+</div>
